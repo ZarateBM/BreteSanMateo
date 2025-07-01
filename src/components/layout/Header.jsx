bd007b07b518f70f071b3e7754597ebeb4cb2c54
¨ZarateBM¨ <brandonzaratem2603@gmail.com>
2025-07-01 03:17:03 -0600

    fix: Actualizar la ruta de la imagen del logo en el componente Header

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -12,7 +12,6 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import logo from '@/public/SanMateoEmpleos.png'; // Import the logo image
 
 const Header = () => {
   const navigate = useNavigate();
@@ -31,7 +30,7 @@ const Header = () => {
             animate={{ rotate: [0, 10, -10, 0] }}
             transition={{ duration: 1, repeat: Infinity, repeatDelay: 3 }}
           >
-            <img src={logo} alt="Tu Brete San Mateo" className="h-24 w-28" />
+            <img src="/SanMateoEmpleos.png" alt="Tu Brete San Mateo" className="h-24 w-28" />
           </motion.div>
         </Link>
         <div className="flex items-center gap-2">
